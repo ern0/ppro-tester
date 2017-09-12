b18bd969a3424954fb1802c82a47eb0f885d556b
ern0 <ern0@linkbroker.hu>
2017-09-12 23:54:42 +0200

    added some int handlers, they will be the solution for hanging illegal instructions

--- tester/tester.asm
+++ tester/tester.asm
@@ -1,5 +1,6 @@
 ;FASM source
 
+;-------------------------------------------------------
 MACRO Print string
 {
  CALL @F
@@ -9,7 +10,7 @@ MACRO Print string
  MOV    AH,9
  INT    21H
 }
-
+;-------------------------------------------------------
 MACRO PrintLN string
 {
  CALL @F
@@ -19,7 +20,7 @@ MACRO PrintLN string
  MOV    AH,9
  INT    21H
 }
-
+;-------------------------------------------------------
 MACRO TCMOVB reg1,reg2
 {
  MOV    reg1,0
@@ -27,11 +28,16 @@ MACRO TCMOVB reg1,reg2
  CMOVB  reg1,reg2
  CMP    reg1,reg2
 }
-
-
+;-------------------------------------------------------
 ORG 256
 
  PrintLN 'PentiumPro instruction set tester'
+
+ CALL setIllegalHandler
+ CALL setTimerHandler
+
+endless:
+ ;JMP    endless
 
  Print  'CMOVB AX,BX - '
  STC
@@ -39,6 +45,8 @@ ORG 256
  JNE    bug
  CLC
  TCMOVB AX,BX
+ JE     bug
+ TEST  [fatal],-1
  JE     bug
  PrintLN 'Ok'
  JMP    next
@@ -48,3 +56,59 @@ next:
 
  MOV    AX,4C00H
  INT    21H
+;-------------------------------------------------------
+; Some BSS
+
+fatal   DB 0
+timerSaveOffs DW 0
+timerSaveSeg  DW 0
+;-------------------------------------------------------
+setIllegalHandler:
+
+ PUSH   DS
+
+ XOR    AX,AX
+ MOV    DS,AX
+
+ MOV    AX,CS
+ MOV    WORD [6 * 4 + 0],AX
+ MOV    WORD [6 * 4 + 2],illegalHandler
+
+ POP    DS
+ RET
+;-------------------------------------------------------
+illegalHandler:
+
+ MOV [fatal],-1
+ IRET
+;-------------------------------------------------------
+setTimerHandler:
+ 
+ PUSH   DS
+
+ XOR    AX,AX
+ MOV    DS,AX
+
+ MOV    AX,WORD [8 * 4 + 0]
+ MOV    [CS:timerSaveOffs],AX
+ MOV    AX,WORD [8 * 4 + 2]
+ MOV    [CS:timerSaveSeg],AX
+
+ CLI
+ MOV    WORD [8 * 4 + 0],timerHandler
+ MOV    AX,CS
+ MOV    WORD [8 * 4 + 2],AX
+ STI
+
+ POP    DS
+ RET
+;-------------------------------------------------------
+timerHandler:
+
+ PUSH   DS
+ PUSH   0b800H
+ POP    DS
+ INC    BYTE [0]
+ POP    DS
+
+ JMP    DWORD [CS:timerSaveOffs]
